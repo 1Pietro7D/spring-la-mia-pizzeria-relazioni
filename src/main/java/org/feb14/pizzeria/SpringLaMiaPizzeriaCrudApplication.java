44be597d632dea32725cd4192146923f65b8ce29
PietroDanieli <1pie7d@gmail.com>
2023-02-14 14:30:55 +0100

    initialize spring project

---
+++ src/main/java/org/feb14/pizzeria/SpringLaMiaPizzeriaCrudApplication.java
@@ -0,0 +1,13 @@
+package org.feb14.pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLaMiaPizzeriaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLaMiaPizzeriaCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/feb14/pizzeria/SpringLaMiaPizzeriaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.feb14.pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLaMiaPizzeriaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
