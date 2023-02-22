4c7b9660e06857c6ef7770aa9001aac704f6081a
PietroDanieli <1pie7d@gmail.com>
2023-02-22 14:28:15 +0100

    refactoring and manegment error

--- src/main/java/org/feb14/pizzeria/controller/PizzaController.java
+++ src/main/java/org/feb14/pizzeria/controller/PizzaController.java
@@ -5,9 +5,7 @@ import java.util.Optional;
 
 import org.feb14.pizzeria.model.Pizza;
 import org.feb14.pizzeria.repository.PizzaRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -44,22 +41,16 @@ public class PizzaController {
 	}
 
 	@GetMapping("/{id}")
-	public String show(@PathVariable("id") String id, Model modShow) {
-		try {
-			Integer validId = Integer.parseInt(id);
-			
-			
-				Optional<Pizza> pizza = pizzaRepository.findById(validId); // restituisce un'istanza Optional con dentro
+	public String show(@PathVariable("id") Integer id, Model modShow) {
+		
+						
+				Optional<Pizza> pizza = pizzaRepository.findById(id); // restituisce un'istanza Optional con dentro
 																			// forse una pizza
-				if(pizza.isPresent()) {
 				modShow.addAttribute("pizza", pizza.get());
 				return "pizze/detail";
-				}
-				return "/errorPage";
 			
-		} catch (NumberFormatException e) {
-			return "/errorPage";
-		}
+			
+		
 	}
 
 	@GetMapping("/create")
@@ -68,7 +59,7 @@ public class PizzaController {
 		return "pizze/create";
 	}
 
-	@PostMapping("/create") // gestirà le richieste di tipo POST di tipo /books/create
+	@PostMapping("/store") // gestirà le richieste di tipo POST di tipo /books/create
 	public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, // TODO : approfondire BindingResult
 			Model modelCreate) {
 
@@ -87,7 +78,7 @@ public class PizzaController {
 		return "pizze/update";
 	}
 	
-	@PostMapping("/edit/{id}")
+	@PostMapping("/update/{id}")
 	public String update(
 			@Valid @ModelAttribute Pizza formPizza,
 			BindingResult result, Model model
